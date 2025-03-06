92bea1979f434f1e606dcf0a8add57e814aff8d9
Espadv69 <aitorjob69@gmail.com>
2025-03-06 18:51:03 +0100

    Implement state management and localStorage integration in WordCounter component

--- src/components/WordCounter.jsx
+++ src/components/WordCounter.jsx
@@ -1,3 +1,8 @@
-const WordCounter = () => {}
+import { useState, useEffect } from 'react'
+import { saveText, getText } from '../utils/localStorage'
+
+const WordCounter = () => {
+  const [text, setText] = useState(getText(''))
+}
 
 export default WordCounter
