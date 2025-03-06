b4d20dfe7a143d036b35b24fe0ecca03fc7be616
Espadv69 <aitorjob69@gmail.com>
2025-03-06 19:50:48 +0100

    Implement ResetButton component functionality with onClick handler

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -1,3 +1,9 @@
-const ResetButton = ({ onReset }) => {}
+const ResetButton = ({ onReset }) => {
+  return (
+    <button onClick={onReset} className="">
+      Reset
+    </button>
+  )
+}
 
 export default ResetButton
