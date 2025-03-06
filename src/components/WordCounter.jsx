36cdc6ddb25725071145a1fd7ff1863af4375439
Espadv69 <aitorjob69@gmail.com>
2025-03-06 18:51:37 +0100

    Add useEffect to save text to localStorage on change in WordCounter component

--- src/components/WordCounter.jsx
+++ src/components/WordCounter.jsx
@@ -3,6 +3,10 @@ import { saveText, getText } from '../utils/localStorage'
 
 const WordCounter = () => {
   const [text, setText] = useState(getText(''))
+
+  useEffect(() => {
+    saveText(text)
+  }, [text])
 }
 
 export default WordCounter
