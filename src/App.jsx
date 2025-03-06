3eb47b535148bfeff8a9494f7f4612b79de6f1c0
Espadv69 <aitorjob69@gmail.com>
2025-03-06 19:10:08 +0100

    Add WordCounter component to App for text analysis functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => {}
+import WordCounter from './components/WordCounter'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <WordCounter />
+    </div>
+  )
+}
 
 export default App
